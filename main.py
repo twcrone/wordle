e43b2be0bd2fcbb0597c0713bca04867d271d375
Todd W Crone <twcrone@gmail.com>
2022-01-30 10:09:08 -0500

    Get the parameter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sys
+
 if __name__ == "__main__":
-    wordFile = open("words.txt", "r")
-    Lines = wordFile.readlines()
-    for line in Lines:
-        print(line)
+    word = "COULD" if len(sys.argv) < 2 else sys.argv[1]
+    print(word)
+    # wordFile = open("words.txt", "r")
+    # Lines = wordFile.readlines()
+    # for line in Lines:
+    #     print(line)
