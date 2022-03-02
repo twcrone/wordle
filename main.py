730816153babb6b4dad1cc3cf29ddd0faa72c894
Todd W Crone <twcrone@gmail.com>
2022-03-02 00:40:14 -0500

    I think it works more betterly

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import random
 import sys
 
 
-def find_best_options(pattern, exclude):
+def find_best_options(pattern, include, exclude):
     best_options = []
-    best_score = 0
     word_file = open("words.txt", "r")
     lines = word_file.readlines()
     if pattern is None:
@@ -14,13 +13,8 @@ def find_best_options(pattern, exclude):
 
     for line in lines:
         line = line.strip()
-        line_score = score(line, pattern, exclude)
-        # print(line + " (" + str(line_score) + ")")
-        if line_score == best_score:
+        if match(line, pattern, include, exclude):
             best_options.append(line)
-        elif line_score > best_score:
-            best_options = [line]
-            best_score = line_score
     return best_options
 
 
@@ -42,26 +36,11 @@ def match(word, exact, include, exclude):
     return True
 
 
-def score(word, pattern, exclude):
-    if len(word) != len(pattern):
-        return -1
-    result = 0
-    for i in range(len(word)):
-        w = word[i]
-        p = pattern[i]
-        if any(letter in word for letter in exclude):
-            return 0
-        elif p.isupper() and w == p.lower():
-            result += 3
-        elif w != p.lower() and p in word:
-            result += 1
-
-    return result
-
-
 if __name__ == "__main__":
     pat = None if len(sys.argv) < 2 else sys.argv[1]
-    exc = "" if len(sys.argv) < 3 else sys.argv[2]
+    inc = "" if len(sys.argv) < 3 else sys.argv[2]
+    exc = "" if len(sys.argv) < 4 else sys.argv[3]
     print(pat)
+    print(inc)
     print(exc)
-    print(find_best_options(pat, exc))
+    print(find_best_options(pat, inc, exc))
